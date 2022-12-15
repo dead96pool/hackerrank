bb640b68ed291dde4865dfc89c912e20b835a823
dead96pool <sachleensingh975@gmail.com>
2022-12-15 16:28:51 +0530

    update

---
+++ Math/mod_divmod.py
@@ -0,0 +1,14 @@
+"""
+
+Mod Divmod
+
+hackerrank link: https://www.hackerrank.com/challenges/python-mod-divmod/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+"""
+
+a = int(input())
+b = int(input())
+
+print(a//b) # integer division
+print(a%b)  # gives remainder
+print(divmod(a,b))
