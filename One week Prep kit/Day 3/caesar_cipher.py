26c3808fb23beefc494b98ae9c350326eaa91c6d
dead96pool <sachleensingh975@gmail.com>
2022-12-27 06:40:18 +0530

    update

---
+++ One week Prep kit/Day 3/caesar_cipher.py
@@ -0,0 +1,50 @@
+"""
+Caesar cipher
+link: https://www.hackerrank.com/challenges/one-week-preparation-kit-caesar-cipher-1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-three
+
+
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'caesarCipher' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. INTEGER k
+#
+
+def caesarCipher(s, k):
+    # Write your code here
+    cipher = ""
+    for char in s:
+        if char.islower():
+            cipher += chr((ord(char)+k-97)%26+97)
+        elif char.isupper():
+            cipher += chr((ord(char)+k-65)%26+65)
+        else:
+            cipher += char
+    return cipher
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = input()
+
+    k = int(input().strip())
+
+    result = caesarCipher(s, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
